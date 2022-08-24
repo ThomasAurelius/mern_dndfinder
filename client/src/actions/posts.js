2936de1c7adfd5ca8d87480f2341f5a2b914d8b6
Thomas Aurelius <teg824@hotmail.com>
2022-08-24 02:05:59 -0500

    initial

---
+++ client/src/actions/posts.js
@@ -0,0 +1,29 @@
+import * as api from  '../api';
+
+export const getPosts = () => async (dispatch) => {
+   try {
+      const { data } = await api.fetchPosts();
+      dispatch({ type: 'FETCH_ALL', payload: data });
+   } catch (error) {
+      console.log(error.message);
+   }
+
+}
+
+export const createPost = (newPost) => async (dispatch) => {
+   try {
+      const { data } = await api.createPost(newPost);
+      dispatch({ type: 'CREATE', payload: data });
+   } catch (error) {
+      console.log(error.message);
+   } 
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+   try {
+      const { data } = await api.updatePost(id, post);
+      dispatch({ type: 'UPDATE', payload: data });
+   } catch (error) {
+      console.log(error.message); 
+   }
+}
---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = "mongodb+srv://dndfinder:dndfinder3451@cluster0.fzk8u.mongodb.net/dndfinder?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+   .catch((error) => console.log(error.message));
+
+
