7097b2bae56c0604fb49a20bf17af0dc25867dfe
Thomas Aurelius <teg824@hotmail.com>
2022-08-27 22:32:52 -0500

    styling

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -63,7 +63,7 @@ const Post = ({ post, setCurrentId }) => {
           <Typography variant="h6">{post.name}</Typography>
           <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
         </div>
-        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator || user?.result?.type === 'admin') && (
         <div className={classes.overlay2} name="edit">
           <Button
             onClick={(e) => {
@@ -99,7 +99,7 @@ const Post = ({ post, setCurrentId }) => {
         <Button size="small" color="primary" disabled={!user?.result} onClick={handleLike}>
           <Likes />
         </Button>
-        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator || user?.result?.type === 'admin') && (
           <Button size="small" color="secondary" onClick={() => dispatch(deletePost(post._id))}>
             <DeleteIcon fontSize="small" /> &nbsp; Delete
           </Button>
