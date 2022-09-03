2ad71ee4f5b59a0cdf6217802638cd372f3123b4
Thomas Aurelius <teg824@hotmail.com>
2022-09-03 02:12:47 -0500

    working on vercel

--- server/index.js
+++ server/index.js
@@ -13,7 +13,13 @@ const app = express();
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-
+ app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
 
 const PORT = process.env.PORT|| 5000;
