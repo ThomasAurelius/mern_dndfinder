b486b85245d61c40b20ca7ce9954c814d382d051
Thomas Aurelius <teg824@hotmail.com>
2022-09-03 16:55:19 -0500

    removed content-type

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Accept, Authorization");
   next();
 });
 
