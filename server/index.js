2b4f4d3832fbe91c413a960934d04e29e4e096bd
Thomas Aurelius <teg824@hotmail.com>
2022-09-03 01:55:22 -0500

    prep for vercel

--- server/index.js
+++ server/index.js
@@ -14,15 +14,15 @@ app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes);
-app.use("/user", userRouter);
 
 
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 // mongoose.set('useFindAndModify', false);
 
+app.use("/posts", postRoutes);
+app.use("/user", userRouter);
