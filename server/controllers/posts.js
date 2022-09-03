8f62ce01b815c2ea688621b41de4d94a0cd465a4
Thomas Aurelius <teg824@hotmail.com>
2022-09-02 23:53:10 -0500

    improved search

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -22,12 +22,18 @@ export const getPosts = async (req, res) => {
 }
 
 export const getPostsBySearch = async (req, res) => {
-    const { searchQuery, tags } = req.query;
+    const { searchQuery, tags} = req.query;
 
     try {
         const title = new RegExp(searchQuery, "i");
+        const message = new RegExp(searchQuery, "i");
+        const city = new RegExp(searchQuery, "i");
+        const daysAvailable = new RegExp(searchQuery, "i");
+        const zipCode = new RegExp(searchQuery, "i");
+        
 
-        const posts = await PostMessage.find({ $or: [ { title }, { tags: { $in: tags.split(',') } } ]});
+
+        const posts = await PostMessage.find({ $or: [ { title }, { message }, { city }, { daysAvailable }, { tags: { $in: tags.split(',') } } ]});
 
         res.json({ data: posts });
     } catch (error) {    
