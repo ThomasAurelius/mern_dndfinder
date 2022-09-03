5ff5177ae61baad463dc9ea8bac300ca4bffd2b8
Thomas Aurelius <teg824@hotmail.com>
2022-09-03 02:17:21 -0500

    testing

--- server/index.js
+++ server/index.js
@@ -9,17 +9,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
+//const cors = require('cors');
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
- app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+//app.use(cors());
+
+const corsOptions ={
+    origin:'mern-dndfinder.vercel.app', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 const PORT = process.env.PORT|| 5000;
