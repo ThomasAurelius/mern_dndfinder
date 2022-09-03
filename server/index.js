187541c34b48c7ad61ff148fb9f64d015c8b213c
Thomas Aurelius <teg824@hotmail.com>
2022-09-03 02:31:26 -0500

    vercel

--- server/index.js
+++ server/index.js
@@ -14,6 +14,12 @@ const cors = require('cors');
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 const corsOptions ={
     origin:'mern-dndfinder.vercel.app', 
